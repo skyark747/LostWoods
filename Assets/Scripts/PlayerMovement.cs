c80939ad6f2e4cc62925d4513ec1315a2f74a07f
Mohammad Soban Ijaz <130098155+skyark747@users.noreply.github.com>
2023-07-31 04:38:12 -0700

    make joystick

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody),typeof(BoxCollider))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Animator animator;
+    [SerializeField] private FixedJoystick js;
+
+    public float speed = 2f;
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(-js.Horizontal * speed, rb.velocity.y, -js.Vertical * speed);
+        if (js.Horizontal != 0 || js.Vertical != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(rb.velocity);
+        }
+        
+    }
+}
