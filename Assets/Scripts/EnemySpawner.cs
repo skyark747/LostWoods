f01c45c2d337b7f95a4afc0b93d9752209d8a525
eren-senpai70 <130178492+eren-senpai70@users.noreply.github.com>
2023-08-03 01:52:10 +0500

    enemy spawned

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject MyEnemy;
+    public int EnemyCount;
+    public int MaxEnemy;
+    public int xPos;
+    public int zPos;
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+   IEnumerator SpawnEnemy()
+   {
+        while(EnemyCount<=10) 
+        {
+
+            xPos = Random.Range(54, 54);
+            zPos = Random.Range(29, 43);
+            Instantiate(MyEnemy, new Vector3(xPos, 2.372385f, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(1);
+            EnemyCount++;
+        }
+   }
+}
