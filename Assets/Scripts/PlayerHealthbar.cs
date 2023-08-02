81ced800d08c2530ddcbf49bc13c1d0b80b08a4f
Mohammad Soban Ijaz <130098155+skyark747@users.noreply.github.com>
2023-08-02 10:49:48 -0700

    add script to health

---
+++ Assets/Scripts/PlayerHealthbar.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthbar : MonoBehaviour
+{
+    [SerializeField] Image img;
+    [SerializeField] float chealth=1000;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("enemy"))
+        {
+            Health();
+        }
+    }
+    void Health()
+    {
+        img.fillAmount = chealth/20;
+    }
+}
