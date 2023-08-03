dfe57c7d8fe2859483b91f9685c52982db043473
Mohammad Soban Ijaz <130098155+skyark747@users.noreply.github.com>
2023-08-03 12:39:55 -0700

    change

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,35 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
     [SerializeField] private Animator animator;
     [SerializeField] private FixedJoystick js;
-    [SerializeField] private LayerMask groundLayer;
-
+    [SerializeField] private LayerMask mask;
+    public  AnimationCurve curve;
     public float speed = 3f;
-
+    bool isgrounded = true;
     private void FixedUpdate()
     {
-        bool isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f, groundLayer);
-        if (isGrounded)
+        movement();
+    }
+    private void Update()
+    {
+        surfaceallignment();
+    }
+    private void movement()
+    {
+        if (isgrounded)
         {
-            transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
-            
-        }
-        rb.velocity = new Vector3(-js.Horizontal * speed, rb.velocity.y, -js.Vertical * speed);
-
-        if (js.Horizontal != 0 || js.Vertical != 0)
-        {
-            transform.rotation = Quaternion.LookRotation(rb.velocity);
-            animator.SetBool("IsWalking", true);
+            rb.velocity = new Vector3(-js.Horizontal * speed, rb.velocity.y, -js.Vertical * speed);
+            if (js.Horizontal != 0 || js.Vertical != 0)
+            {
+                transform.rotation = Quaternion.LookRotation(rb.velocity);
+                animator.SetBool("IsWalking", true);
+            }
+            else
+            {
+                animator.SetBool("IsWalking", false);
+            }
         }
         else
         {
             animator.SetBool("IsWalking", false);
+        }
+    }
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    isgrounded = true;
+    //}
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    isgrounded=false;
+    //}
+    private void surfaceallignment()
+    {
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit Rc = new RaycastHit();
+
+        if (Physics.Raycast(ray, out Rc, 0.1f, mask))
+        {
+            Quaternion.Lerp(transform.rotation, Quaternion.FromToRotation(Vector3.up, Rc.normal), curve.Evaluate(0.25f));
         }
     }
 }
