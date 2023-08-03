aeb24fcb9d1a3cb1cde9af44c2bd2a09252bd26a
eren-senpai70 <130178492+eren-senpai70@users.noreply.github.com>
2023-08-03 07:32:02 +0500

    joystick animation set

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private FixedJoystick _joystick;
+    [SerializeField] private Animator _animator;
+
+    [SerializeField] private float moveSpeed;
+
+    private void Update()
+    {
+        rb.velocity = new Vector3(_joystick.Horizontal * moveSpeed, rb.velocity.y, _joystick.Vertical * moveSpeed);
+
+        if (_joystick.Horizontal != 0 && _joystick.Vertical != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(rb.velocity);
+            _animator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            _animator.SetBool("IsWalking", false);
+        }
+    }
+}
