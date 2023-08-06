e5c7aa73ebaa95336ba8e31c91ecd4270d65b574
Mohammad Soban Ijaz <130098155+skyark747@users.noreply.github.com>
2023-08-06 08:56:21 -0700

    ok make changes

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Vector3 set;
+    [SerializeField] Transform target;
+
+
+    void Start()
+    {
+        set = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x + set.x, target.position.y + set.y, target.position.z + set.z );
+
+
+    }
+}
