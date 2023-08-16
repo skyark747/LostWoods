eb92bfef641bf6089265360c7a252ee956886592
Mohammad Soban Ijaz <130098155+skyark747@users.noreply.github.com>
2023-08-16 09:04:27 -0700

    laser done

---
+++ Assets/Scripts/GunLaser.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunLaser : MonoBehaviour
+{
+    private LineRenderer lr;
+    void Start()
+    {
+        lr=GetComponent<LineRenderer>();
+    }
+
+    
+    void Update()
+    {
+        lr.SetPosition(0,transform.position);
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position,transform.forward,out hit))
+        {
+            if(hit.collider)
+            {
+                lr.SetPosition(1, hit.point);
+            }
+        }
+        lr.SetPosition(1, transform.forward *100);
+        
+    }
+}
