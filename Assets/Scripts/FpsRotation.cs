66743ac1e748dd31b8ee10fa28e40b08f38f6640
Mohammad Soban Ijaz <130098155+skyark747@users.noreply.github.com>
2023-08-19 07:27:49 -0700

    changed the code for rotation

--- Assets/Scripts/FpsRotation.cs
+++ Assets/Scripts/FpsRotation.cs
@@ -1,29 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.DeviceSimulation;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class FpsRotation : MonoBehaviour
 {
-    private Vector3 lastMousePosition;
-    public float rotationSpeed = 1.0f;
+    private Vector2 touchStartPos;
+    private float rotationSpeed = 1.0f;
 
     void Update()
     {
-        Vector3 currentMousePosition = Input.mousePosition;
-
-        if (Input.GetMouseButtonDown(0))
+        if (Input.touchCount > 0)
         {
-            lastMousePosition = currentMousePosition;
-        }
-        else if (Input.GetMouseButton(0))
-        {
-            Vector3 deltaMousePos = currentMousePosition - lastMousePosition;
-            float rotationAmount = deltaMousePos.x * rotationSpeed;
+            Touch touch = Input.GetTouch(0);
 
-            transform.Rotate(Vector3.up, rotationAmount, Space.World);
+            if (touch.phase == UnityEngine.TouchPhase.Began)
+            {
+                touchStartPos = touch.position;
+            }
+            else if (touch.phase == UnityEngine.TouchPhase.Moved)
+            {
+                Vector2 deltaTouchPos = touch.position - touchStartPos;
+                float rotationAmount = deltaTouchPos.x * rotationSpeed;
 
-            lastMousePosition = currentMousePosition;
+                transform.Rotate(Vector3.up, rotationAmount, Space.World);
+
+                touchStartPos = touch.position;
+            }
         }
+
+        //private Vector3 lastMousePosition;
+        //public float rotationSpeed = 1.0f;
+
+        //void Update()
+        //{
+        //    Vector3 currentMousePosition = Input.mousePosition;
+
+        //    if (Input.GetMouseButtonDown(0))
+        //    {
+        //        lastMousePosition = currentMousePosition;
+        //    }
+        //    else if (Input.GetMouseButton(0))
+        //    {
+        //        Vector3 deltaMousePos = currentMousePosition - lastMousePosition;
+        //        float rotationAmount = deltaMousePos.x * rotationSpeed;
+
+        //        transform.Rotate(Vector3.up, rotationAmount, Space.World);
+
+        //        lastMousePosition = currentMousePosition;
+        //    }
+        //}
     }
-}
